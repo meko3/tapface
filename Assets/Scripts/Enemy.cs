14286750e12daf12cb3d5b6a27866847a2c090bd
meko3 <hirono.yoneyama@outlook.com>
2018-08-21 00:32:32 +0900

    [build]: init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : Token {
+
+	// 生存数
+  	public static int Count = 0;
+
+	// Use this for initialization.
+	void Start () {
+
+		// 生存数を増やす
+    	Count++;
+
+		// Set size.
+		SetSize(SpriteWidth / 2, SpriteHeight / 2);
+
+		// move to random direction.
+		// determine direction.
+		float dir = Random.Range(0, 359);
+		// determine speed 2.
+		float spd = 2;
+		SetVelocity(dir, spd);
+		Debug.Log(dir);
+	}
+
+	// update
+	void Update () {
+		// get left bottom side coordinate
+		Vector2 min = GetWorldMin();
+		// get right up side coordinate
+		Vector2 max = GetWorldMax();
+
+		if (X < min.x || max.x < X) {
+			// if out of field , change direction
+			VX *= -1;
+			// move inside display
+			ClampScreen();
+		}
+
+		if (Y < min.y || max.y < Y) {
+			// if out of field , change direction
+			VY *= -1;
+			// move inside display
+			ClampScreen();
+		}
+	}
+
+	public void OnMouseDown() {
+
+		// 生存数を減らす
+    	Count--;
+
+		// delete Object
+		DestroyObj();
+
+		// パーティクルを生成
+    	for (int i = 0; i < 16; i++)
+    	{
+      		Particle.Add(X, Y);
+    	}
+
+	}
+
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# 勉強用
+## 参考サイト
+`https://qiita.com/2dgames_jp/items/11bb76167fb44bb5af5f`
+
+## インストール
+`git clone https://github.com/meko3/tapface.git`
+
+## 動作環境
+`MacOS High Sierra バージョン 10.13.6`
+
+## 使用フレームワーク
+`Unity`
